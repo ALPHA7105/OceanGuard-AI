62e8e8841fe42b43e7a6f3e8ae0a5ee483150a82
ALPHA7105 <ssworld7105@gmail.com>
2026-01-05 21:37:09 +0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 
-
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      define: {
+        'process.env': {} 
+      },
       server: {
         port: 3000,
         host: '0.0.0.0',
