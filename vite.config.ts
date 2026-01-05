9d6a8cdb4a1fd98149702a8c4f40dcd3e89d431b
ALPHA7105 <ssworld7105@gmail.com>
2026-01-05 22:32:05 +0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,14 @@
+import { defineConfig } from 'vite';
 import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      define: {
-        'process.env': {} 
-      },
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [],
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
+export default defineConfig({
+  server: {
+    port: 3000,
+    host: '0.0.0.0',
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '.'),
+    }
+  }
 });
